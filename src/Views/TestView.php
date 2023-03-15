6baf185573168258d84b34885bd3d1a87dc1c263
ttcchhmm <60962195+ttcchhmm@users.noreply.github.com>
2023-03-15 15:54:02 +0100

    Fix broken indentation

--- src/Views/TestView.php
+++ src/Views/TestView.php
@@ -4,7 +4,7 @@ declare(strict_types = 1);
 namespace PokeWeb\Views;
 
 class TestView implements IView {
-	public function display(array $options): string {
+    public function display(array $options): string {
         ob_start();
 
         echo '<pre>';
@@ -12,5 +12,5 @@ class TestView implements IView {
         echo '</pre>';
 
         return ob_get_clean();
-	}
+    }
 }
